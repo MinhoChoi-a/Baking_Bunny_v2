048053ad5b8afa9e1896a15bae9c2cfb13d55011
MinhoChoi-a <minhocalgary@gmail.com>
2021-05-11 11:00:07 -0600

    set build env & proxy

--- bakingbunny-v2-back/app.js
+++ bakingbunny-v2-back/app.js
@@ -5,6 +5,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+//const cors = require('cors') / app.use(cors())
 
 var mongoose = require('mongoose');
 
@@ -26,7 +27,8 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('build'));
+//app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
