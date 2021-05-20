0fb5025e3b8697246bc16e55e3b12dde00bf755f
MinhoChoi-a <minhocalgary@gmail.com>
2021-05-20 14:35:59 -0600

    setting for jwt(token)

--- bakingbunny-v2-back/controllers/CustomerController.js
+++ bakingbunny-v2-back/controllers/CustomerController.js
@@ -1,6 +1,36 @@
 const bcrypt = require('bcrypt')
-const { response } = require('../app')
+const jwt = require('jsonwebtoken')
+
 const Customer = require('../models/Customer')
+
+
+exports.signin = async (req, res, next) => {
+
+    const body = request.body
+
+    const user = await Customer.findOne({username: body.username})
+    const passwordCorrect = user === null
+        ? false
+        : await bcrypt.compare(body.password, user.passwordHash)
+
+    if(!(user && passwordCorrect)) {
+        return res.status(401).json({
+            error: 'invalid username or password'
+        })
+    }
+
+    const userToken = {
+        username: user.username,
+        id: user._id,
+    }
+    
+    const token = jwt.sign(userToken, process.env.SECRET)
+
+    response
+        .status(200)
+        .send({token, username: user.username, name: user.name})
+
+}
 
 
 exports.signon = async (req, res, next) => {
--- bakingbunny-v2-back/controllers/ItemController.js
+++ bakingbunny-v2-back/controllers/ItemController.js
@@ -1,3 +1,6 @@
+const jwt = require('jsonwebtoken')
+const Customer = require('../models/Customer')
+
 exports.getMenu = (req, res, next) => {
 
     const dt = req.params.date;
@@ -17,3 +20,33 @@ exports.getMenu = (req, res, next) => {
     }
     
 };
+
+const getTokenFrom = request => {
+
+    const authorization = request.get('authorization')
+    if(authorization && authorization.toLowerCase().startWith('bearer ')) {
+        return authorization.substring(7)
+    }
+
+    return null
+
+}
+
+//using jwt
+exports.order = async (req, res, next) => {
+
+    const body = req.body
+    const token = getTokenFrom(req)
+    const decodedToken = jwt.verify(token, process.env.SECRET)
+
+    if(!token || !decodedToken.id) {
+
+        return res.status(401).json({error: 'token missing or invalid'})
+
+    }
+
+    //any code of method for authorized user
+    const user = await Customer.findById(decodedToken.id)
+    
+    res.json('something')
+}
