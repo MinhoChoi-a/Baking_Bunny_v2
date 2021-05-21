2849473729eea50e31d9abe94bf6383433cd0b2f
MinhoChoi-a <minhocalgary@gmail.com>
2021-05-21 15:01:34 -0600

    set & store token on frontend side

---
+++ bakingbunny-v2-front/service/testToken.js
@@ -0,0 +1,21 @@
+import axios from 'axios'
+const baseUrl = '/api/notes'
+
+let token = null
+
+const setToken = newToken => {
+    token = `bearer ${newToken}`
+}
+
+const create = async newObject => {
+    
+    const config = {
+        headers: {Authorization: token},
+    }
+
+    const response = await axios.post(baseUrl, newObject, config)
+
+    return response.data
+}
+
+export default {setToken, create}
--- bakingbunny-v2-front/src/App.js
+++ bakingbunny-v2-front/src/App.js
@@ -1,25 +1,108 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import loginService from '../service/login'
+import tokenService from '../service/testToken'
+
+const App = () => {
+
+  const [errorMessage, setErrorMessage] = useState(null)
+  const [username, setUsername] = useState('')   
+  const [password, setPassword] = useState('') 
+  const [user, setUser] = userState(null)
+
+  useEffect(() => {
+
+  }, [])
+
+  useEffect(() => {
+
+    const loggedUserJSON = window.localStorage.getItem('loggedAppUser')
+    if(loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+      tokenService.setToken(user.token)
+    }
+
+  }, []) //empty array ensures that this effect should be executed only when the component is rendered for the first time.
+
+
+  const handleLogin = (event) => {
+    event.preventDefault()
+    
+    try {
+      const user = await loginService.login({
+        username, password,
+      })
+
+      window.localStorage.setItem(
+        'loggedAppUser', JSON.stringify(user)
+      )
+      //security issue of XSS(cross site scripting)
+      /*
+      When using React in a sensible manner it should not be possible since React sanitizes all text that it renders, meaning that it is not executing the rendered content as JavaScript.
+      */
+      tokenService.setToken(user.token)
+      
+      setUser(user)
+      setUsername('')
+      setPassword('')
+    } catch (exception) {
+      setErrorMessage('Wrong Credentials')
+      setTimeout(() => {
+        setErrorMessage(null)
+      }, 5000)
+    }
+  }
+
+  const handleLogout = () => {
+    setUser(null)
+    window.localStorage.removeItem('loggedAppUser')
+    window.localStorage.clear()
+  }
+
+  const loginForm = () => {
+
+    <div>
+      <h1>Login Test</h1>
+      
+      <Notification message={errorMessage} />
+      
+      <form onSubmit={handleLogin}>
+        <div>
+          username
+          <input type="text" value={username} name="username" onChange={({target}) => setUsername(target.value)}/>
+        </div>
+        <div>
+          password
+          <input type="text" value={password} name="password" onChange={({target}) => setPassword(target.value)}/>
+        </div>
+        <button type="submit">login</button>
+      </form>
+
     </div>
-  );
-}
+  }
+
+  const afterLoginForm =() => {
+    <div>
+    <h1>Logged in</h1>
+    
+    <button type="button" onClick={() => handleLogout()}>logout</button>
+    </div>
+  }
+    
+  return (
+    <div>
+      <h1>Baking bunny</h1>
+    
+    {
+      user === null ?
+      loginForm :
+      afterLoginForm
+    }
+    
+    </div>
+  )
+ }
 
 export default App;
