927f3579587c930842016e2a383b078defa98fde
MinhoChoi-a <minhocalgary@gmail.com>
2021-08-11 17:40:42 -0600

    sample of custom hook

--- bakingbunny-v2-front/src/App.js
+++ bakingbunny-v2-front/src/App.js
@@ -177,6 +177,33 @@ const App = () => {
 </div>
 </Router>*/
 
+/*
+custom hook sample
+
+const useCounter = () => {
+  const [value, setValue] = useState(0)
+
+  const increase = () => {
+    setValue(value + 1)
+  }
+
+  const decrease = () => {
+    setValue(value - 1)
+  }
+
+  const zero = () => {
+    setValue(0)
+  }
+
+  return {
+    value, 
+    increase,
+    decrease,
+    zero
+  }
+}
+
+*/
 
 
   )
